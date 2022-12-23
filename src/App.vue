da394a96209878f853c381edfe6ada0ecb887153
1914k <k_1914@163.com>
2022-12-23 15:51:52 +0800

    基础目录构建

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,57 @@
 <template>
   <div id="app">
     <h1>APP</h1>
-    <router-view/>
+    <div>{{a}}</div>
+    <button class="test" @click="test">测试</button>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<script lang="ts">
+// optionsAPI 写法
+import Vue from 'vue'
 
+export default Vue.extend({
+  data () {
+    return {
+      a: 1,
+      b: '2',
+      c: [],
+      d: {
+        a: 1,
+        b: '2'
+      }
+    }
+  },
+  methods: {
+    test () {
+      this.a.toString()
+    }
+  }
+})
+// classAPIs 写法
+// import Vue from 'vue'
+// import Component from 'vue-class-component'
+// @Component
+// export default class App extends Vue {
+//   a = 1
+//   b = '2'
+//   c = []
+//   d = {
+//     a: 1,
+//     b: '2'
+//   }
+
+//   test ():void {
+//     console.log(this.a)
+//   }
+// }
+</script>
+
+<style lang="scss">
+// @import '~@/styles/variables.scss';
+
+.test {
+  color: $warning-color;
+}
 </style>
